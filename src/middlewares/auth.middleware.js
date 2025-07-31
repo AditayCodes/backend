cd17516d98217e002eb77e7e113baf8017d4c0f7
ADITAY SHARMA <137498537+AditaySharma18@users.noreply.github.com>
2025-07-31 20:04:38 +0530

    all midddleware fixed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,30 +1,27 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
-import jwt from "jsonwebtoken";
-import { User } from "../models/user.model.js";
+import JWT from 'jsonwebtoken';
+import asyncHandler from '../utils/asyncHandler.js';
+import ApiError from '../utils/apiError.js';
+import { User } from '../models/user.model.js';
 
-
-export const verifyJWT = asyncHandler(async (req, res, next) => {
+export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
-        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
         if (!accessToken) {
-            throw new ApiError(401, "Unauthorization request")
+            throw new ApiError(401, "Unauthorized request");
         }
     
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const decodedToken = JWT.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
     
-        const user = await User.findById(decodedToken?._id).
-            select("-password -refreshToken")
-        
+        const user = await User.findById(decodedToken?._id).select(" -password -refreshToken");
+    
         if (!user) {
-            throw new ApiError(401, "Invalid Access Token")
+            throw new ApiError(401, "Invalid access token");
         }
     
         req.user = user;
-        next()
-    
+        next();
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid Access Token")
+        throw new ApiError(401, error?.message || "Invalid access token");
     }
-
 });
